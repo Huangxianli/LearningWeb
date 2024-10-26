0774b8e3d0802fb0d51e944631767782d907df57
huangxianli <huangxainli.email.com>
2024-10-26 14:08:37 +0800

    array

--- javaScript/es6/array/content/instanceFunction/index.js
+++ javaScript/es6/array/content/instanceFunction/index.js
@@ -6,20 +6,24 @@ import test_every from './every.js';
 import test_fill from './fill.js';
 import test_filter from './filter.js';
 import test_find from './find.js';
+import test_findIndex from './findIndex.js';
+import test_findLast from './findLast.js';
 
 /**
  * 1、作用和使用方法
  * 2、是否修改原数组
  * 3、empty的影响，是否不会忽略empty
- * 4、是否通用，使用的条件
- * 5、使用途中修改数组里面的内容，修改数组长度有影响吗
+ * 4、使用途中修改数组里面的内容，修改数组长度有影响吗
+ * 5、是否通用，使用的条件
  */
 
 // test_at();
 // test_concat();
 // test_copyWithin();
-test_entries();
+// test_entries();
 // test_every();
 // test_fill();
 // test_filter();
 // test_find();
+// test_findIndex();
+test_findLast();
