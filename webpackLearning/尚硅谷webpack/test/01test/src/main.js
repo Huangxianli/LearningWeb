1ac0c7a36b79c43c7582a676ae6d0e48257a9ffb
huangxianli <huangxainli.email.com>
2024-12-24 20:14:45 +0800

    webpack learning 基础

---
+++ webpackLearning/尚硅谷webpack/test/01test/src/main.js
@@ -0,0 +1,4 @@
+import { multiply } from './a';
+import { count } from './b';
+multiply(1, 1);
+count(1, 2);
---
+++ webpackLearning/尚硅谷webpack/test/02test/src/main.js
@@ -0,0 +1,4 @@
+import { a } from './a';
+import { b } from './b';
+import './a.css';
+console.log(a, b); 
