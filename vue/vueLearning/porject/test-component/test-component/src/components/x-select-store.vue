1955f6763b007af9250e8be383cff03a1b761203
huangxianli <huangxainli.email.com>
2025-01-10 22:53:51 +0800

    组价练习

--- vue/vueLearning/porject/test-component/test-component/src/components/x-select-store.vue
+++ vue/vueLearning/porject/test-component/test-component/src/components/x-select-store.vue
@@ -1,11 +1,28 @@
 <template>
-  <el-select size="mini" v-bind="$attrs" v-on="$listeners">
-    <el-option v-for=" option in store.data" :key="option[keyMapKey]" :label="option[labelMapKey]"
-      :value="option[valueMapKey]"></el-option>
-  </el-select>
+  <div>
+    <el-select size="mini" v-bind="$attrs" v-on="$listeners">
+      <template #empty>
+        <div class="empty-option-box">
+          <div v-if="store.status === REQUEST_STATUS.START" class="empty-option-box-content" v-loading="true">
+          </div>
+          <div v-else-if="store.status === REQUEST_STATUS.FAIL" class="empty-option-box-content">
+            <div>数据请求失败，请重试</div>
+            <el-button class="empty-option-box-retry" size="mini" @click="clickReload">重试</el-button>
+          </div>
+          <div v-else class="empty-option-box-content">暂无数据</div>
+        </div>
+      </template>
+      <template #default>
+        <el-option v-for=" option in store.data" :key="option[keyMapKey]" :label="option[labelMapKey]"
+          :value="option[valueMapKey]"></el-option>
+      </template>
+    </el-select>
+  </div>
+
 </template>
 
 <script>
+import { REQUEST_STATUS } from '@/components/constant.js'
 export default {
   props: {
     store: {
@@ -27,12 +44,45 @@ export default {
       type: String,
     }
   },
+  computed: {
+    REQUEST_STATUS () {
+      return REQUEST_STATUS;
+    }
+  },
+
   created () {
     if (this.store.autoLoad) {
+      this.store.load();
+    }
+  },
+  methods: {
+    clickReload () {
       this.store.load();
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+.empty-option-box {
+  height: 100px;
+}
+
+.empty-option-loading-box {
+  height: 100%;
+}
+
+.empty-option-box-content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #999;
+  font-size: 14px;
+}
+
+.empty-option-box-retry {
+  margin-top: 12px;
+}
+</style>
