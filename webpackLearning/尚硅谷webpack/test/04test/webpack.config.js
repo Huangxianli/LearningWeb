3cf7a85e500fba3cd1d119a9b01566e789f1170c
huangxianli <huangxainli.email.com>
2025-02-16 23:09:53 +0800

    'webpack

--- webpackLearning/尚硅谷webpack/test/04test/webpack.config.js
+++ webpackLearning/尚硅谷webpack/test/04test/webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
   // devServer: {
   //   host: "localhost", // 启动服务器域名
   //   port: "3000", // 启动服务器端口号
-  //   open: true, // 是否自动打开浏览器
+  //   open: true, // 是否自动打开浏览器 
   // },
   devServer: {
     host: "localhost", // 启动服务器域名
