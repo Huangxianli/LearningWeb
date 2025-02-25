09169e52b913a5aa1eed9ebbb9b07896234db5d2
huangxianli <huangxainli.email.com>
2025-02-25 22:41:30 +0800

    vuex learning

---
+++ vue/vueLearning/porject/vuex-learning/vuex-learning/src/views/list/ListChecked.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list-checked">
+    <div class="list-checked-title">
+      已经选中的项目
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.list-checked {
+  margin: 5px;
+  padding: 5px;
+  border: 1px dotted rgb(47, 0, 255);
+}
+
+.list-checked-title {
+  color: gray;
+  border-bottom: 1px dotted gray;
+  margin-bottom: 5px
+}
+</style>
---
+++ vue/vueLearning/porject/vuex-learning/vuex-learning/src/views/list/ListSearchResult.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-sreach-reasult">
+    <div class="list-sreach-reasult-title">
+      搜索结果
+    </div>
+    <ListSearchResultRow />
+  </div>
+</template>
+
+<script>
+import ListSearchResultRow from './ListSearchResultRow.vue';
+export default {
+  components: {
+    ListSearchResultRow,
+  }
+}
+</script>
+
+<style scoped>
+.list-sreach-reasult {
+  margin: 5px;
+  padding: 5px;
+  border: 1px dotted rgb(47, 0, 255);
+}
+
+.list-sreach-reasult-title {
+  color: gray;
+  border-bottom: 1px dotted gray;
+  margin-bottom: 5px
+}
+</style>
