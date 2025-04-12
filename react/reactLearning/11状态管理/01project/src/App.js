a96e10759f838213716e8f651b82750687172b34
huangxianli <huangxainli.email.com>
2025-04-12 22:57:44 +0800

    React 复习

--- react/reactLearning/11状态管理/01project/src/App.js
+++ react/reactLearning/11状态管理/01project/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 
 import ReactingToInputWithState from './reactingToInputWithState/index.js';
 import ChoosingTheStateStructure from './choosingTheStateStructure';
+import PreservingAndResettingState from './preservingAndResettingState/index.js';
 import ExtractingStateLogicIntoAReducer from './extractingStateLogicIntoAReducer/index.js';
-import PassingDataDeeplyWithContext from './passingDataDeeplyWithContext'
+import PassingDataDeeplyWithContext from './passingDataDeeplyWithContext';
 import ScalingUpWithReducerAndContext from './scalingUpWithReducerAndContext/index.js';
-function App () {
+function App() {
   return (
     <div className="App">
       <ReactingToInputWithState />
       <ChoosingTheStateStructure />
+      <PreservingAndResettingState />
       <ExtractingStateLogicIntoAReducer />
       <PassingDataDeeplyWithContext />
       <ScalingUpWithReducerAndContext />
