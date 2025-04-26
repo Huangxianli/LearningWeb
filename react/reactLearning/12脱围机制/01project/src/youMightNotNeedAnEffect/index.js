11a6daa1d3d6a54375daef60b16efeb889d5d503
zhishiwo <2025311667@qq.com>
2025-04-27 07:38:12 +0800

    react

--- react/reactLearning/12脱围机制/01project/src/youMightNotNeedAnEffect/index.js
+++ react/reactLearning/12脱围机制/01project/src/youMightNotNeedAnEffect/index.js
@@ -1,20 +1,21 @@
-/* 你可能不需要Effect */
+/* 你可能不需要 Effect */
 
-import UpdatingStateBasedOnPropsOrState from "./01UpdatingStateBasedOnPropsOrState";
+import UpdatingStateBasedOnPropsOrState from './01UpdatingStateBasedOnPropsOrState';
 /**
- * 如果没有涉及到外部系统，就不应该使用Effect
+ * 如果没有涉及到外部系统，就不应该使用 Effect
  */
+
 /**
- * 如何移除不必要的Effect
- * 不必使用Effect来转换渲染所需要的数据（在Effect选中重新设置state等）
- * 不必使用Effect来处理用户事件
+ * 如何移除不必要的 Effect
+ * 不必使用 Effect 来转换渲染所需要的数据（在 Effect 选中重新设置 state 等）
+ * 不必使用 Effect 来处理用户事件
  */
-import Container from "../components/Container";
-function YouMightNotNeedAnEffect () {
+import Container from '../components/Container';
+function YouMightNotNeedAnEffect() {
   return (
-    <Container title='你可能不需要Effect'>
+    <Container title="你可能不需要 Effect">
       <UpdatingStateBasedOnPropsOrState />
     </Container>
   );
-};
+}
 export default YouMightNotNeedAnEffect;
