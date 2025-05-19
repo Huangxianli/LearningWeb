4fcc2a589fabdfb601fe023a5f24ad3aaf7037c0
huangxianli <huangxainli.email.com>
2025-05-19 14:32:12 +0800

    vue3 列表渲染 事件处理 模板引用 组件基础 注册

--- vue3/vue3Learning/12 组件基础/Component1.js
+++ vue3/vue3Learning/12 组件基础/Component1.js
@@ -1,13 +1,13 @@
 export default {
   props: ['title'], // 使用的不是 <script setup>，所以这里需要使用 props 来声明接收的参数
   // emits: ['myClick'], // 使用的不是 <script setup>，也不是直接在 template 中 $emit，所以这里需要使用 emits 来声明派发的事件；但是实际上写不写好像都可以，主要是提供类型检查等功能
-  setup (props, ctx) {
+  setup(props, ctx) {
     const handleClick = (e) => {
       ctx.emit('myClick', e);
-    }
+    };
     return {
       props,
-      handleClick
+      handleClick,
     };
   },
   template: `
@@ -16,4 +16,4 @@ export default {
     <br />
     {{props.title}}-{{title}}
   `,
-}
+};
